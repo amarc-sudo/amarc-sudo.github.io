d7b53eec2a8d2c930e47e0f016922c6972679ede
Aurelien <aurelien_marc@icloud.com>
2022-10-18 10:11:09 +0200

    git init branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InformationComponent } from './information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    InformationComponent
+   ],
   imports: [
     BrowserModule
   ],
